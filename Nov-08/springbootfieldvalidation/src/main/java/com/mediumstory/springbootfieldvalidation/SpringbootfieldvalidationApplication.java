ee948e3d0f172ab1c72ce54ef43d1563de62751a
Nivi-01 <niveda.ec19@bitsathy.ac.in>
2022-11-23 20:18:17 +0530

    hello

---
+++ Nov-08/springbootfieldvalidation/src/main/java/com/mediumstory/springbootfieldvalidation/SpringbootfieldvalidationApplication.java
@@ -0,0 +1,13 @@
+package com.mediumstory.springbootfieldvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootfieldvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootfieldvalidationApplication.class, args);
+	}
+
+}
